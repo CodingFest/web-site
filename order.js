ec589b8567753eea8ecfad1ecc92f1cae0e99a75
Andrei <europeoplebelarus@gmail.com>
2017-08-30 10:44:38 +0300

    Add files via upload

---
+++ order.js
@@ -0,0 +1,13 @@
+﻿var dialog = document.querySelector('dialog');
+      document.querySelector('#show').onclick = function() {
+        dialog.show();
+      };
+      document.querySelector('#close').onclick = function() {
+        dialog.close();
+      };
+
+      function order(){
+        var o = document.getElementById("window")
+        o.innerHTML="Ваш заказ принят!"
+        setTimeout(function(){$('.window').fadeOut('fast')},5000);
+      }
